5fca9ef52f80babf409e0d8745718b974aef506c
vbrnbs <varszegibarnabas@gmail.com>
2023-06-25 20:40:15 +1200

    auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
  
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import { useUser } from "@clerk/clerk-react";
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
-
   const user = useUser();
 
   return (
@@ -14,7 +14,6 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}
-
       </main>
     </>
   );
