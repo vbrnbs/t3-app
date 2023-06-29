dd8e8002c2891dda99a37228171aa0b58ba68d6a
vbrnbs <varszegibarnabas@gmail.com>
2023-06-30 10:05:39 +1200

    Router type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import Head from "next/head";
 import { useUser } from "@clerk/clerk-react";
 import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import { api } from "~/utils/api";
+import { Post } from "@prisma/client";
 
 export default function Home() {
   const user = useUser();
-  
-  const { data } = api.post.getAll.useQuery(); 
+
+  const { data } = api.post.getAll.useQuery();
   return (
     <>
       <Head>
@@ -17,7 +18,9 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}
         <div>
-          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+          {data?.map((post: Post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
