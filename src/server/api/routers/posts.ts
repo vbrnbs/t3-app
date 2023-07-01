f3efd445c290d570fad0c69c0eaf2bdf7ce53ff3
vbrnbs <varszegibarnabas@gmail.com>
2023-07-01 13:14:24 +1200

    Next Image, style up

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,49 @@
+import { User } from "@clerk/nextjs/api";
+import { clerkClient } from '@clerk/nextjs/server';
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+}
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorID),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      {
+        const author = users.find((user) => user.id === post.authorID);
+        console.log(author);
+
+        if (!author || !author.username) throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found"
+        });
+
+        return {
+          post,
+          author
+          // : {
+          //   ...author,
+          //   username: author.username,
+          // },
+        };
+      }
+    });
   }),
 });
